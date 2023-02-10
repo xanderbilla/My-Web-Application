b277f84c061430cbcb702bc502e958b440e4f91b
Vikas Singh <xanderbilla@gmail.com>
2023-02-11 00:56:09 +0200

    Updated About Page

--- client/src/pages/about/About.js
+++ client/src/pages/about/About.js
@@ -2,35 +2,35 @@ import React from 'react'
 import './about.css'
 const About = () => {
     return (
-        <main class="container">
+        <main className="container">
             <h1 className='display-5 fw-bold'>About Me</h1>
-            <p class="col-md-10 fs-5">Myself <strong>Vikas Singh</strong> from Varanasi, Uttar Pradesh. Learning and exploring technical domains at <strong>Acharya Institute, Bangalore (IN)</strong> from the last two years.</p>
-            <p class="col-md-8 mb-5 fs-5">The main goal is to learn as much domains, tools and techs I can and I will.</p>
-            <hr class="col-3 col-md-2 mb-5" />
+            <p className="col-md-10 fs-5">Myself <strong>Vikas Singh</strong> from Varanasi, Uttar Pradesh. Learning and exploring technical domains at <strong>Acharya Institute, Bangalore (IN)</strong> from the last two years.</p>
+            <p className="col-md-8 mb-5 fs-5">The main goal is to learn as much domains, tools and techs I can and I will.</p>
+            <hr className="col-3 col-md-2 mb-5" />
 
-            <div class="row g-5">
-                <div class="col-md-6">
-                    <h2 className='display-6 fw-semibold'>Skills</h2>
-                    <p class="fs-5">So, far I traveled this much... still have a long way to go.</p>
-                    <ul class="icon-list ps-0">
-                        <li class="d-flex align-items-start mb-1 fs-5">AWS Management</li>
-                        <li class="d-flex align-items-start mb-1 fs-5">Database (SQL & MongoDB)</li>
-                        <li class="d-flex align-items-start mb-1 fs-5">Java</li>
-                        <li class="d-flex align-items-start mb-1 fs-5">Python</li>
-                        <li class="d-flex align-items-start mb-1 fs-5">Networking (CCNA & CCNP)</li>
-                        <li class="d-flex align-items-start mb-1 fs-5">Web Development</li>
+            <div className="row g-5">
+                <div className="col-md-6">
+                    <h2 className='display-7 fw-semibold'>Skills</h2>
+                    <p>So, far I traveled this much... still have a long way to go.</p>
+                    <ul className="icon-list ps-0">
+                        <li className="d-flex align-items-start mb-1">AWS Management</li>
+                        <li className="d-flex align-items-start mb-1">Database (SQL & MongoDB)</li>
+                        <li className="d-flex align-items-start mb-1">Java</li>
+                        <li className="d-flex align-items-start mb-1">Python</li>
+                        <li className="d-flex align-items-start mb-1">Networking (CCNA & CCNP)</li>
+                        <li className="d-flex align-items-start mb-1">Web Development</li>
                     </ul>
                 </div>
 
-                <div class="col-md-6">
-                <h2 className='display-6 fw-semibold'>Projects</h2>
-                    <p class="fs-5">Check out the projects I had made and the upcoming projects. All are web application projects.</p>
-                    <ul class="icon-list ps-0">
-                        <li class="d-flex align-items-start mb-1 fs-5"><a class="text-decoration-none" href="https://xanderbilla-static-portfolio.vercel.app/">Single Static Page Using HTML and CSS</a></li>
-                        <li class="d-flex align-items-start mb-1 fs-5"><a class="text-decoration-none" href="https://promsnat-aws.s3.ap-south-1.amazonaws.com/Image-Slider+-+JavaScript/index.html">Image Slider Using JavaScript</a></li>
-                        <li class="d-flex align-items-start mb-1 fs-5"><a class="text-decoration-none" href="https://xanderbilla-react-portfolio-i.vercel.app/">Portfolio Using React</a></li>
-                        <li class="d-flex align-items-start mb-1 fs-5"><a class="text-decoration-none" href="https://xanderbilla-react-bootstrap-web-application.vercel.app/">Basic Web Application Using Bootstrap and React</a></li>
-                        <li class="text-muted d-flex align-items-start mb-1 fs-5">Netflix Clone Using React (Coming Soon)</li>
+                <div className="col-md-6">
+                <h2 className='display-7 fw-semibold'>Projects</h2>
+                    <p>Check out the projects I had made and the upcoming projects. All are web application projects.</p>
+                    <ul className="icon-list ps-0">
+                        <li className="d-flex align-items-start mb-1"><a className="text-decoration-none" href="https://xanderbilla-static-portfolio.vercel.app/">Single Static Page Using HTML and CSS</a></li>
+                        <li className="d-flex align-items-start mb-1"><a className="text-decoration-none" href="https://promsnat-aws.s3.ap-south-1.amazonaws.com/Image-Slider+-+JavaScript/index.html">Image Slider Using JavaScript</a></li>
+                        <li className="d-flex align-items-start mb-1"><a className="text-decoration-none" href="https://xanderbilla-react-portfolio-i.vercel.app/">Portfolio Using React</a></li>
+                        <li className="d-flex align-items-start mb-1"><a className="text-decoration-none" href="https://xanderbilla-react-bootstrap-web-application.vercel.app/">Basic Web Application Using Bootstrap and React</a></li>
+                        <li className="text-muted d-flex align-items-start mb-1">Netflix Clone Using React (Coming Soon)</li>
                     </ul>
                 </div>
             </div>
