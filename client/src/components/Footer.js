bcf77098f4c041ad50d477d4071f11f7d8cd8b4c
Vikas Singh <xanderbilla@gmail.com>
2023-02-11 14:40:25 +0200

    Updated Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { IconContext } from "react-icons";
-import { FaDiscord, FaLinkedin, FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
+import { FaDiscord, FaLinkedin, FaInstagram, FaYoutube } from "react-icons/fa";
 import Container from 'react-bootstrap/esm/Container';
 
 function Footer() {
@@ -11,7 +11,6 @@ function Footer() {
         <div className='d-flex justify-content-center'>
           <IconContext.Provider value={{ size: "2em", color: "black", style: { marginRight: '0.8rem' } }}>
             <a href="https://discord.com/channels/@me/843582508124995605"><FaDiscord /></a>
-            <a href="https://github.com/xanderbilla"><FaGithub /></a>
             <a href="https://www.instagram.com/xander.billa"><FaInstagram /></a>
             <a href="https://www.linkedin.com/in/vikas-singh-212278165/"><FaLinkedin /></a>
             <a href="https://youtube.com/xanderbilla"><FaYoutube /></a>
