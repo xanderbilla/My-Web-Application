f14369b0e11e480ce784a2c39fe68c36f1369e5e
Vikas Singh <xanderbilla@gmail.com>
2023-02-13 15:07:25 +0200

    Updated Application Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,44 @@
+import { createContext, useReducer } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
-
 import Navbar from './components/Navbar';
 import About from './pages/about/About';
 import Contact from './pages/contact/Contact';
-import Error404 from './pages/Error404';
+import ErrorPage from './pages/error/ErrorPage';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Logout from './pages/logout/Logout';
 import Registration from './pages/registration/Registration';
 import Support from './pages/support/Support';
+import { initialState, reducer } from './reducer/UseReducer';
+// Context API
+export const UserContext = createContext();
 
-function App() {
+const Routing = () => {
+  return (<>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/logout" element={<Logout />} />
+      <Route path="/signIn" element={<Login />} />
+      <Route path="/signUp" element={<Registration />} />
+      <Route path="/support" element={<Support />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  </>)
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
   return (
     <>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/signIn" element={<Login/>}/>
-          <Route path="/signUp" element={<Registration/>}/>
-          <Route path="/support" element={<Support />} />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
-      <Footer />
+        <UserContext.Provider value={{ state, dispatch }}>
+          <Routing/>
+        </UserContext.Provider>
+        <Footer />
       </BrowserRouter>
     </>
   );
