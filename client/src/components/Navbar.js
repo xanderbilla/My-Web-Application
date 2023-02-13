88e6504ea6aa326e841b15a0be4384b184c8535a
Vikas Singh <xanderbilla@gmail.com>
2023-02-13 15:03:18 +0200

    Updated Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Brand from '../assets/Brand'
 import { IconContext } from "react-icons";
 import { FaGithub } from "react-icons/fa";
 import { Link } from 'react-router-dom';
-import {Nav} from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
 import './navbar.css'
+import { UserContext } from '../App';
+
 const Navbar = () => {
+    const { state, dispatch } = useContext(UserContext);
+    const RenderMenu = () => {
+        if (state) {
+            return (
+                <>
+                    <Link type="submit" className="btn btn-outline-secondary text-decoration-none" to="/logout">Logout</Link>
+                </>
+            )
+        } else {
+            return (<>
+                <Link type="submit" className="me-2 btn btn-outline-dark text-decoration-none" to="/signIn">Sign In</Link>
+                <Link type="submit" className="me-2 btn btn-dark text-decoration-none" to="/signUp">Sign Up</Link>
+            </>)
+        }
+    }
     return (
         <div className='container' >
             <div className="d-flex flex-column flex-md-row align-items-center mt-md-3 pb-3 mb-3 border-bottom">
-                <a href="/" className="d-flex align-items-center text-decoration-none">
+                <Link to="/" className="d-flex align-items-center text-decoration-none">
                     <Brand height={55} />
-                </a>
+                </Link>
 
                 <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto navigation">
                     <Nav.Link><Link className="me-3 py-2 text-dark text-decoration-none" to="/">Home</Link></Nav.Link>
@@ -19,12 +36,11 @@ const Navbar = () => {
                     <Nav.Link><Link className="me-3 py-2 text-dark text-decoration-none" to="/contact">Contact</Link></Nav.Link>
                     <Nav.Link><Link className="me-3 py-2 text-dark text-decoration-none" to="/support">Support</Link></Nav.Link>
                 </nav>
-                <nav className="d--inline-flex ms-md-auto">
+                <nav className="d-inline-flex ms-md-auto">
                     <IconContext.Provider value={{ size: "2em", color: "black", }}>
-                        <a className="me-2" href="https://github.com/xanderbilla"><FaGithub /></a>
+                        <Link className="me-2" to="https://github.com/xanderbilla"><FaGithub /></Link>
                     </IconContext.Provider>
-                    <Link type="submit" className="me-2 btn btn-outline-dark text-decoration-none" to="/signIn">Sign In</Link>
-                    <Link type="submit" className="btn btn-dark text-decoration-none" to="/signUp">Sign Up</Link>
+                    <RenderMenu />
                 </nav>
             </div>
         </div>
